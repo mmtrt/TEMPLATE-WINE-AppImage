61dd1eb6d521fb3d39b848b69501126365fc471d
Taqi Raza <mttalpur@gmail.com>
2026-07-03 16:43:33 +0500

    Initial commit

---
+++ get-dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm wine cabextract sdl2 pipewire-audio pipewire-jack harfbuzz gst-plugins-bad gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly gst-libav gstreamer 7zip unzip
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+
+if [ "$ARCH" = 'x86_64' ]; then
+	sudo pacman -S --noconfirm mingw-w64-binutils
+fi
+
+# Comment this out if you need an AUR package
+make-aur-package zenity-rs-bin
+
+# If the application needs to be manually built that has to be done down here
